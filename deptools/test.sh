342fc62cc250aa57d3e1d778daa5be2fb579db11
Christophe Guillon <christophe.guillon.perso@gmail.com>
2009-10-27 01:24:04 +0100

    Added some information at end of test.sh

--- deptools/test.sh
+++ deptools/test.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+dir=`dirname $0`
 res=0
-for i in plugins/test_*.sh
+for i in $dir/plugins/test_*.sh
 do
     $i
     [ $? = 0 ] || res=1
 done
+echo
+[ $res = 0 ] && echo "PASSED: all tests passed"
 [ $res = 0 ] || echo "FAILED: some tests failed"
 exit $res
